46852ed5ee0528ecc4b4443f5168cd5db317fbcf
Deyvison Silva <de.yvison@hotmail.com>
2026-04-30 09:25:33 -0300

    Opções de personalização do Admin

---
+++ apps/api/src/tenants/dto/branding.dto.ts
@@ -0,0 +1,19 @@
+﻿import { IsString, IsOptional, IsHexColor } from 'class-validator';
+
+export class UpdateBrandingDto {
+  @IsOptional()
+  @IsString()
+  logoUrl?: string;
+
+  @IsOptional()
+  @IsHexColor()
+  primaryColor?: string;
+
+  @IsOptional()
+  @IsHexColor()
+  secondaryColor?: string;
+
+  @IsOptional()
+  @IsString()
+  customDomain?: string;
+}
